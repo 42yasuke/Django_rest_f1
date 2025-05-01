4a8a1cf0a1bc3128a3107bbc1acdad4b3693fa45
42yasuke <j.ralph1998@gmail.com>
2025-05-01 14:29:50 +0200

    part 2

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import PostViewSet, UserLoginApiView
 
 router = DefaultRouter()
-router.register('posts', PostViewSet, 'post')
+router.register('posts', PostViewSet)
 
 urlpatterns = [
 	path('', include(router.urls)),
+	path('login/', UserLoginApiView.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,21 @@
 from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken 
+from rest_framework.settings import api_settings
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Post
 from .serializers import PostSerializer
-
+from .permissions import UpdateOwnProfile
 
 class PostViewSet(viewsets.ModelViewSet):
 	serializer_class = PostSerializer
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (
+        UpdateOwnProfile,
+        IsAuthenticatedOrReadOnly,
+    )
 	queryset = Post.objects.all()
+
+class UserLoginApiView(ObtainAuthToken):
+	renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
